945245e45381e5487d90412342b92fc848615be1
b10902026-jimmy <b10902026@mail.ntust.edu.tw>
2023-05-27 09:43:09 +0000

    add_test_unit

--- pkg/upload/handleUpload_test.go
+++ pkg/upload/handleUpload_test.go
@@ -12,7 +12,14 @@ import (
 func TestHandleUpload(t *testing.T) {
 	var buf bytes.Buffer
 	mw := multipart.NewWriter(&buf)
-	_ = mw.WriteField("video_file", "test video file content")
+	fw, err := mw.CreateFormFile("video_file", "filename.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = fw.Write([]byte("test video file content"))
+	if err != nil {
+		t.Fatal(err)
+	}
 	mw.Close()
 
 	req := httptest.NewRequest(http.MethodPost, "/upload", &buf)
@@ -22,8 +29,13 @@ func TestHandleUpload(t *testing.T) {
 	os.Setenv("WHISPER_API_KEY", "test_api_key")
 
 	worker := Worker{
-		// 填入適當的工作人員數據
+		JobQueue: make(chan Job, 1), // 創建一個帶有緩衝區的 channel
 	}
+	// 在後台讀取 job，以便測試可以完成
+	go func() {
+		<-worker.JobQueue
+	}()
+
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		HandleUpload(w, r, worker)
@@ -35,4 +47,5 @@ func TestHandleUpload(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+
 }
