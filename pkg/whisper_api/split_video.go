b0d846cfab1010953a9ba761e8bef9baeeaea9f7
b10902026-jimmy <b10902026@mail.ntust.edu.tw>
2023-08-27 16:33:52 +0000

    multi_split

--- pkg/whisper_api/split_video.go
+++ pkg/whisper_api/split_video.go
@@ -2,7 +2,6 @@ package whisper_api
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"videoUploadAndProcessing/pkg/audio_processing"
 )
@@ -12,7 +11,7 @@ import (
 
 func SplitVideoIntoSegmentsByTimestamps(videoPath string, sentenceTimestamps []SentenceTimestamp, videoDuration float64) ([]string, []string, error) {
 	var allSegmentPaths []string
-	var voiceSegmentPaths []string // 用於存儲僅包含語音的片段
+	var voiceSegmentPaths []string
 	const outputDir = "../pkg/audio_processing/tmp/video/"
 
 	// 檢查目錄是否存在，如果不存在則創建
@@ -23,44 +22,46 @@ func SplitVideoIntoSegmentsByTimestamps(videoPath string, sentenceTimestamps []S
 		}
 	}
 
-	// 預設上一個段落的結束時間為0
+	// Prepare segment times for FFmpeg command
+	var segmentTimes []float64
 	lastEndTime := 0.0
-
 	for i, ts := range sentenceTimestamps {
 		// 如果當前片段的開始時間不是緊接在上一個片段的結束時間後，提取中間的片段
 		if ts.StartTime > lastEndTime {
 			gapOutputFile := outputDir + fmt.Sprintf("video_gap_segment%d.mp4", i)
-			err := audio_processing.ExecFFMPEG("-y", "-i", videoPath, "-ss", fmt.Sprint(lastEndTime), "-t", fmt.Sprint(ts.StartTime-lastEndTime), gapOutputFile)
-			if err != nil {
-				return nil, nil, fmt.Errorf("error extracting gap segment: %v", err)
-			}
+			segmentTimes = append(segmentTimes, ts.StartTime)
 			allSegmentPaths = append(allSegmentPaths, gapOutputFile)
-			log.Println("Added gap segment path:", gapOutputFile)
 		}
 
-		// 提取有對應時間戳的片段
+		// Prepare for voice segments
 		outputFile := outputDir + fmt.Sprintf("video_segment%d.mp4", i)
-		err := audio_processing.ExecFFMPEG("-y", "-i", videoPath, "-ss", fmt.Sprint(ts.StartTime), "-t", fmt.Sprint(ts.EndTime-ts.StartTime), outputFile)
-		if err != nil {
-			return nil, nil, fmt.Errorf("error splitting video into segments: %v", err)
-		}
+		segmentTimes = append(segmentTimes, ts.EndTime)
+		allSegmentPaths = append(allSegmentPaths, outputFile)
+		voiceSegmentPaths = append(voiceSegmentPaths, outputFile)
 
-		allSegmentPaths = append(allSegmentPaths, outputFile)     // 添加到所有片段的列表中
-		voiceSegmentPaths = append(voiceSegmentPaths, outputFile) // 添加到語音片段的列表中
-		log.Println("Added voice segment path:", outputFile)
-		// 更新上一段的結束時間
+		// Update the last end time
 		lastEndTime = ts.EndTime
 	}
 
-	// 處理影片的最後部分，如果有的話
+	// Handle the end part of the video
 	if lastEndTime < videoDuration {
-		gapOutputFile := outputDir + "video_end_segment.mp4"
-		err := audio_processing.ExecFFMPEG("-y", "-i", videoPath, "-ss", fmt.Sprint(lastEndTime), "-t", fmt.Sprint(videoDuration-lastEndTime), gapOutputFile)
-		if err != nil {
-			return nil, nil, fmt.Errorf("error extracting end segment: %v", err)
+		endOutputFile := outputDir + "video_end_segment.mp4"
+		allSegmentPaths = append(allSegmentPaths, endOutputFile)
+	}
+
+	// Convert segment times to string format
+	segmentTimesStr := ""
+	for i, t := range segmentTimes {
+		if i > 0 {
+			segmentTimesStr += ","
 		}
-		allSegmentPaths = append(allSegmentPaths, gapOutputFile)
-		log.Println("Added end segment path:", gapOutputFile)
+		segmentTimesStr += fmt.Sprintf("%f", t)
+	}
+
+	// Execute FFmpeg command
+	err := audio_processing.ExecFFMPEG("-i", videoPath, "-c", "copy", "-map", "0", "-f", "segment", "-segment_times", segmentTimesStr, outputDir+"segment%d.mp4")
+	if err != nil {
+		return nil, nil, fmt.Errorf("error executing FFmpeg command: %v", err)
 	}
 
 	return allSegmentPaths, voiceSegmentPaths, nil
