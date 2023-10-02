a76508758abd0c3f0cc1a43d6071dabcb5fc78b8
b10902026-jimmy <b10902026@mail.ntust.edu.tw>
2023-10-02 11:43:52 +0000

    1002-update .gitignore

--- pkg/video_processing/merge_video_segments.go
+++ pkg/video_processing/merge_video_segments.go
@@ -77,8 +77,8 @@ func MergeAllVideoSegmentsTogether(segmentPaths []string) (string, error) {
 		}
 	}
 
-	//Merge all segments into final output and store at /pkg/video_processing/test_files
-	finalVideoDir := "/home/user/videoUploadAndProcessing_go/pkg/video_processing/test_files"
+	//Merge all segments into final output and store at /pkg/video_processing/final_output
+	finalVideoDir := "/home/user/videoUploadAndProcessing_go/pkg/video_processing/final_output"
 
 	// Ensure directory exists
 	if _, err := os.Stat(finalVideoDir); os.IsNotExist(err) {
