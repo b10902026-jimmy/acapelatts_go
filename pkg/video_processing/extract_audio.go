e74b988cc044c5af13cfb7d9a8b0dc117c4a387e
b10902026-jimmy <b10902026@mail.ntust.edu.tw>
2023-10-02 11:43:52 +0000

    1002-update .gitignore

--- pkg/video_processing/extract_audio.go
+++ pkg/video_processing/extract_audio.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"io"
 	"log"
-	"os"
 	"os/exec"
 )
 
@@ -18,17 +17,17 @@ func StreamedExtractAudioFromVideo(filePath string) (io.Reader, error) {
 	}
 
 	// 保存MP3文件到當前工作目錄
-	mp3File, err := os.Create("output.mp3")
+	/*mp3File, err := os.Create("output.mp3")
 	if err != nil {
 		log.Printf("Failed to create MP3 file: %v", err)
 		return nil, err
 	}
-	defer mp3File.Close()
+	defer mp3File.Close()*/
 
 	var buf bytes.Buffer
 	go func() {
 		// 同時將音頻數據寫入Buffer和MP3文件
-		multiWriter := io.MultiWriter(&buf, mp3File)
+		multiWriter := io.MultiWriter(&buf)
 		io.Copy(multiWriter, stdoutPipe)
 	}()
 
